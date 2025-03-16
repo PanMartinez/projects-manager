368798f14ca169b91aeb6ed66815419d27b027f5
PanMartinez <marcinplotka88@gmail.com>
2025-03-16 16:09:39 +0100

    feat(Api): http error handler

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from projects_manager.config.db import engine, SessionLocal
 from projects_manager.config.settings import get_settings
 from projects_manager.domain.common.models import Base
+from projects_manager.handlers import http_error_handler
 
 
 def get_application() -> FastAPI:
     application = FastAPI()
     Base.metadata.create_all(bind=engine)
+    application.add_exception_handler(Exception, http_error_handler)
     application.add_middleware(
         CORSMiddleware,
         allow_origins=get_settings().allowed_hosts,
